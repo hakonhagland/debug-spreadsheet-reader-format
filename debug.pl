97fd0fb9d25185bdd89fe816219af98b1731a4ec
Håkon Hægland <hakon.hagland@gmail.com>
2020-07-09 17:54:51 +0200

    Update.

--- debug.pl
+++ debug.pl
@@ -8,13 +8,16 @@ use POSIX qw( floor fmod );
 use DateTimeXFormatExcel;
 {
     say "DateTime::VERSION : ", $DateTime::VERSION;
-    *DateTime::_format_nanosecs = \&format_nanosecs;
+    no warnings 'redefine';
+    *DateTime::_format_nanosecs = \&format_nanosecs2;
+    *DateTime::format_cldr = \&format_cldr;
     my @args_list = ('system_type', 'apple_excel');
     my $converter = DateTimeXFormatExcel->new( @args_list );
     my $num = "0.112311";
     my $dt = $converter->parse_datetime( $num );
     my $nanosecs = $dt->{rd_nanosecs};
     say "nanosecs: '$nanosecs'";
+    $DB::single = 1;
     my $calc_sub_secs = $dt->format_cldr( "S" );
     if( "0.$calc_sub_secs" >= 0.5 ){
         $dt->subtract( seconds => 1 );
@@ -48,3 +51,57 @@ sub format_nanosecs {
         $formatted_ns
     );
 }
+
+sub format_nanosecs2 {
+    my $self = shift;
+    my $precision = @_ ? shift : 9;
+    say "Precision: '$precision'";
+
+    my $divide_by = 10**( 9 - $precision );
+    say "divide_by: '$divide_by'";
+
+    return sprintf(
+        '%0' . $precision . 'u',
+        floor( $self->{rd_nanosecs} / $divide_by )
+    );
+}
+
+sub format_cldr {
+        my $self = shift;
+
+        say "Format_cldr..";
+        # make a copy or caller's scalars get munged
+        my @p = @_;
+
+        my @r;
+        foreach my $p (@p) {
+            $p =~ s/\G
+                    (?:
+                      '((?:[^']|'')*)' # quote escaped bit of text
+                                       # it needs to end with one
+                                       # quote not followed by
+                                       # another
+                      |
+                      (([a-zA-Z])\3*)     # could be a pattern
+                      |
+                      (.)                 # anything else
+                    )
+                   /
+                    defined $1
+                    ? $1
+                    : defined $2
+                    ? $self->_cldr_pattern($2)
+                    : defined $4
+                    ? $4
+                    : undef # should never get here
+                   /sgex;
+
+            $p =~ s/\'\'/\'/g;
+
+            return $p unless wantarray;
+
+            push @r, $p;
+        }
+
+        return @r;
+    }
